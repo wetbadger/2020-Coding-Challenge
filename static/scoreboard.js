0a434ef6353c0187c992f9aba4a3d44a4c4dd710
Ben Alexander <wetbadger49@gmail.com>
2025-08-15 14:07:02 -0400

    solution

--- static/scoreboard.js
+++ static/scoreboard.js
@@ -1,12 +1,35 @@
+function sortRow(row) {
+  var currentScore = parseInt(row.find('.col-md-2:first').text());
+  var $higherRows = $('.row').not(row).filter(function() {
+    return parseInt($(this).find('.col-md-2:first').text()) <= currentScore;
+  });
+  if ($higherRows.length > 0) {
+    row.insertBefore($higherRows.first());
+  }
+}
+
+function sortAllRows() {
+  var $rows = $(".row").detach(); 
+
+  var sortedRows = $rows.toArray().sort(function(a, b) {
+    var scoreA = parseInt($(a).find(".col-md-2:first").text());
+    var scoreB = parseInt($(b).find(".col-md-2:first").text());
+    return scoreB - scoreA;
+  });
+
+  $("#teams").append(sortedRows);
+}
+
 function display_scoreboard(scoreboard){
   $("#teams").empty();
   $.each(scoreboard, function(index, team){
-    addTeamView(team.id, team.name, team.score);
+    addTeamView(team.id, team.name, team.score, index);
   });
+  sortAllRows();
 }
 
-function addTeamView(id, name, score){
-  var team_template = $("<div class = row></div>");
+function addTeamView(id, name, score, index){
+  var team_template = $(`<div class="row" data-id="${id}"></div>`);
   var name_template = $("<div class = col-md-5></div>");
   var score_template = $("<div class = col-md-2></div>");
   var button_template = $("<div class = col-md-2></div>");
@@ -32,7 +55,10 @@ function increase_score(id){
     contentType: "application/json; charset=utf-8",
     data : JSON.stringify(team_id),
     success: function(result){
-        
+      var teamRow = $(`.row[data-id="${id}"]`);
+      var scoreEl = teamRow.find(".col-md-2:first");
+      scoreEl.text(parseInt(scoreEl.text()) + 1);
+      sortRow(teamRow);
     },
     error: function(request, status, error){
         console.log("Error");
